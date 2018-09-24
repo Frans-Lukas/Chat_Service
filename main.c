59f240dc63df882b02d6b8bd7de9f00bc23add40
Jakub Jagiello <kubajagiello94@gmail.com>
2018-09-24 17:47:25 +0200

    added build_words function

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "pdu_handler/pdu_handler_server-nameserver.h"
 
 int main() {
-    printf("Hello, World!\n");
+    char *test_string = "abcdefgh";
+    uint32_t *test_word = build_words(test_string, 4);
+    printf("%s" "\n", (char*)&test_word[0]);
+    printf("%s" "\n",(char*)&test_word[1]);
     return 0;
 }
--- pdu_handler/pdu_handler_server-nameserver.h
+++ pdu_handler/pdu_handler_server-nameserver.h
@@ -1,11 +1,10 @@
-//
-// Created by c16fld on 2018-09-24.
-//
 
 #ifndef CHAT_SERVICE_PDU_HANDLER_SERVER_NAMESERVER_H
 #define CHAT_SERVICE_PDU_HANDLER_SERVER_NAMESERVER_H
 #include "pdu_helper.h"
+#include "../error_handling.h"
 #include <string.h>
+#include <inttypes.h>
 #define OP_REG 0
 #define OP_ACK 1
 #define OP_ALIVE 2
@@ -13,10 +12,12 @@
 
 
 typedef struct reg{
-    __uint8_t code;
-    __uint8_t server_name_length;
-    __uint16_t tcp_port;
-    __uint32_t server_name [];
+    uint8_t code;
+    uint8_t server_name_length;
+    uint16_t tcp_port;
+    uint32_t *server_name;
 } reg;
-
+uint32_t *build_words(char* input_string, int bytes);
+reg* create_REG(char* server_name, int tcp_port);
 #endif //CHAT_SERVICE_PDU_HANDLER_SERVER_NAMESERVER_H
+
