308837153926c84b751b35ad64dfcfac4e424687
Jakub Jagiello <kubajagiello94@gmail.com>
2018-09-24 14:09:03 +0200

    saving progress

--- pdu_handler/pdu_handler_server-nameserver.c
+++ pdu_handler/pdu_handler_server-nameserver.c
@@ -1,5 +1,27 @@
-//
-// Created by c16fld on 2018-09-24.
-//
 
+#include <stdio.h>
 #include "pdu_handler_server-nameserver.h"
+
+
+
+reg* create_REG(char* server_name, int tcp_port){
+    reg *pdu_reg = (reg*)calloc(1, sizeof(reg));
+    pdu_reg->code = OP_REG;
+
+    int server_name_len = (int) strlen(server_name);
+    if(server_name_len > sizeof(__uint8_t)) {
+        fprintf(stderr, "server_name to long");
+        return NULL;
+    }
+
+    pdu_reg->server_name_length = (__uint8_t) server_name_len;
+    if(tcp_port > sizeof(__uint16_t)){
+        fprintf(stderr, "port to big");
+        return NULL;
+    }
+    pdu_reg->tcp_port = (__uint16_t) tcp_port;
+
+
+
+    return pdu_reg;
+}
--- pdu_handler/pdu_handler_server-nameserver.h
+++ pdu_handler/pdu_handler_server-nameserver.h
@@ -5,10 +5,18 @@
 #ifndef CHAT_SERVICE_PDU_HANDLER_SERVER_NAMESERVER_H
 #define CHAT_SERVICE_PDU_HANDLER_SERVER_NAMESERVER_H
 #include "pdu_helper.h"
-
+#include <string.h>
 #define OP_REG 0
 #define OP_ACK 1
 #define OP_ALIVE 2
 #define OP_NOTREG 100
+
+
+typedef struct reg{
+    __uint8_t code;
+    __uint8_t server_name_length;
+    __uint16_t tcp_port;
+    __uint32_t server_name [];
+} reg;
 
 #endif //CHAT_SERVICE_PDU_HANDLER_SERVER_NAMESERVER_H
