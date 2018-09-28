81738aa197e1caa84f12ae114105d5e2bb35e1c1
Jakub Jagiello <kubajagiello94@gmail.com>
2018-09-28 11:58:28 +0200

    created new directory for server-nameserver pdu's

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include "pdu_handler/pdu_handler_server-nameserver.h"
+#include "pdu_handler/server-nameserver/pdu_handler_server-nameserver.h"
 #include "pdu_handler/pdu_helper.h"
-#include "pdu_handler/test_pdu_handler_server-nameserver.h"
+#include "pdu_handler/server-nameserver/test_pdu_handler_server-nameserver.h"
 #include "pdu_handler/pdu_tests.h"
 
 int main() {
--- pdu_handler/pdu_tests.c
+++ pdu_handler/pdu_tests.c
@@ -1,16 +1,14 @@
 
-#include "test_pdu_handler_server-nameserver.h"
-//
-// Created by c16fld on 2018-09-26.
-//
+#include "server-nameserver/test_pdu_handler_server-nameserver.h"
+
 #include "pdu_tests.h"
-#include "pdu_handler_server-nameserver.h"
+#include "server-nameserver/pdu_handler_server-nameserver.h"
 
 void run_all_tests(){
     fprintf(stderr, "Starting tests...\n");
-    //run_pdu_join_tests();
-    //run_pdu_quit_tests();
-    //run_pdu_participants_tests();
-    run_all_reg_test();
+    run_pdu_join_tests();
+    run_pdu_quit_tests();
+    run_pdu_participants_tests();
+    run_all_server_nameserver_tests();
     fprintf(stderr, "All tests successful!\n");
 }
