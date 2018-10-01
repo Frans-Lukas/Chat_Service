2799503047103000831024ed10e8ac41eb207be8
Jakub Jagiello <kubajagiello94@gmail.com>
2018-10-01 14:58:00 +0200

    Merge branch 'master' of https://github.com/Frans-Lukas/Chat_Service
    # Conflicts:
    #	pdu_handler/client-server/pdu_handler_client-server.h

--- pdu_handler/client-server/pdu_handler_client-server.c
+++ pdu_handler/client-server/pdu_handler_client-server.c
@@ -94,7 +94,7 @@ pdu_mess *pdu_mess_create(char *identity, char *message) {
     pdu->identity_length = (uint8_t) strlen(identity);
     pdu->checksum = create_checksum(message);
     pdu->message_length = (uint16_t) strlen(message);
-    pdu->timestamp = (uint32_t) time;
+    pdu->timestamp = (uint32_t) time(NULL);
     pdu->message = build_words(message, 4);
     pdu->client_identity = build_words(identity, 4);
 }
@@ -148,7 +148,7 @@ pdu_pleave *pdu_pleave_create(char *identity) {
     pdu_pleave *pdu = calloc(1, sizeof(pdu_pleave));
     pdu->op = OP_PLEAVE;
     pdu->identity_length = (uint8_t) strlen(identity);
-    pdu->timestamp = (uint32_t) time;
+    pdu->timestamp = (uint32_t) time(NULL);
     pdu->client_identity = build_words(identity, 4);
 }
 
@@ -178,7 +178,7 @@ pdu_pjoin *pdu_pjoin_create(char *identity) {
     pdu_pjoin *pdu = calloc(1, sizeof(pdu_pjoin));
     pdu->op = OP_PJOIN;
     pdu->identity_length = (uint8_t) strlen(identity);
-    pdu->timestamp = (uint32_t) time;
+    pdu->timestamp = (uint32_t) time(NULL);
     pdu->client_identity = build_words(identity, 4);
 }
 
