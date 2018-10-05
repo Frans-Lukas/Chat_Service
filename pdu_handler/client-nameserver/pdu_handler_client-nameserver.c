62c43435ebe82c7f11e198d76d0a6b163a5948bb
Jakub Jagiello <kubajagiello94@gmail.com>
2018-10-05 10:04:36 +0200

    ntohs, htons done

--- pdu_handler/client-nameserver/pdu_handler_client-nameserver.c
+++ pdu_handler/client-nameserver/pdu_handler_client-nameserver.c
@@ -1,13 +1,16 @@
 
 #include <zconf.h>
+#include <netinet/in.h>
 #include "pdu_handler_client-nameserver.h"
 
 int s_list_serialize(s_list *pdu, char **data) {
     int size = 12 + (get_num_words(pdu->server_name_length, 4) * 4);
     *data = calloc(1, sizeof(s_list));
     *data[0] = OP_SLIST;
+    pdu->number_of_servers = htons(pdu->number_of_servers);
     pdu_cpy_chars(*data + 2, &pdu->number_of_servers, 0, 2);
     pdu_cpy_chars(*data + 4, &pdu->adress, 0, 4);
+    pdu->port = htons(pdu->port);
     pdu_cpy_chars(*data + 8, &pdu->port, 0, 2);
     pdu_cpy_chars(*data + 10, &pdu->number_of_clients, 0, 1);
     pdu_cpy_chars(*data + 11, &pdu->server_name_length, 0, 1);
@@ -21,9 +24,11 @@ s_list *s_list_deserialize(int fd) {
     read_from_fd(fd, &pdu->pdu.op, 1);
     read_from_fd(fd, &pdu->pad, 1);
     read_from_fd(fd, &pdu->number_of_servers, 2);
+    pdu->number_of_servers = ntohs(pdu->number_of_servers);
     pdu->server_name = calloc(pdu->server_name_length, sizeof(uint8_t));
     read_from_fd(fd, &pdu->adress, 4);
     read_from_fd(fd, &pdu->port, 2);
+    pdu->port = ntohs(pdu->port);
     read_from_fd(fd, &pdu->number_of_clients, 1);
     read_from_fd(fd, &pdu->server_name_length, 1);
     read_from_fd(fd, pdu->server_name, pdu->server_name_length);
--- pdu_handler/server-nameserver/pdu_handler_server-nameserver.c
+++ pdu_handler/server-nameserver/pdu_handler_server-nameserver.c
@@ -9,6 +9,7 @@ int not_reg_serialize(not_reg *pdu, char** data) {
     *data = calloc(4, sizeof(uint8_t));
     *data[0] = OP_NOTREG;
     pdu_cpy_chars(*data + 1, &pdu->pad, 0, 1);
+    pdu->id_number = htons(pdu->id_number);
     pdu_cpy_chars(*data + 2, &pdu->id_number, 0, 2);
     return size;
 }
@@ -18,6 +19,7 @@ not_reg *not_reg_deserialize(int fd) {
     pdu->pdu.op = OP_NOTREG;
     read_from_fd(fd, &pdu->pad, 1);
     read_from_fd(fd, &pdu->id_number, 2);
+    pdu->id_number = ntohs(pdu->id_number);
     return pdu;
 }
 
@@ -26,6 +28,7 @@ int ack_serialize(ack *pdu, char** data) {
     *data[0] = OP_ACK;
     *data[1] = pdu->pad;
     pdu_cpy_chars(*data + 1, &pdu->pad, 0, 1);
+    pdu->id_number = htons(pdu->id_number);
     pdu_cpy_chars(*data + 2, &pdu->id_number, 0, 2);
     return 4;
 }
@@ -35,6 +38,7 @@ ack *ack_deserialize(int fd) {
     pdu->pdu.op = OP_ACK;
     read_from_fd(fd, &pdu->pad, 1);
     read_from_fd(fd, &pdu->id_number, 2);
+    pdu->id_number = ntohs(pdu->id_number);
     return pdu;
 }
 
@@ -43,6 +47,7 @@ int alive_serialize(alive *pdu, char** data) {
     *data = calloc(size, sizeof(uint8_t));
     *data[0] = OP_ALIVE;
     pdu_cpy_chars(*data + 1, &pdu->nr_of_clients, 0, 1);
+    pdu->id_number = htons(pdu->id_number);
     pdu_cpy_chars(*data + 2, &pdu->id_number, 0, 2);
     return size;
 }
@@ -52,6 +57,7 @@ alive *alive_deserialize(int fd) {
     pdu->pdu.op = OP_ALIVE;
     read_from_fd(fd, &pdu->nr_of_clients, 1);
     read_from_fd(fd, &pdu->id_number, 2);
+    pdu->id_number = ntohs(pdu->id_number);
     return pdu;
 }
 
@@ -60,6 +66,7 @@ int reg_serialize(reg *pdu, char** data) {
     *data = calloc(1, (size_t) size);
     *data[0] = OP_REG;
     pdu_cpy_chars((*data) + 1, &pdu->server_name_length, 0, 1);
+    pdu->tcp_port = htons(pdu->tcp_port);
     pdu_cpy_chars((*data) + 2, &pdu->tcp_port, 0, 2);
     pdu_cpy_chars((*data) + 4, pdu->server_name, 0, pdu->server_name_length);
     return size;
@@ -70,6 +77,7 @@ reg* reg_deserialize(int fd) {
     pdu->pdu.op = OP_REG;
     read_from_fd(fd, &pdu->server_name_length, 1);
     read_from_fd(fd, &pdu->tcp_port, 2);
+    pdu->tcp_port = ntohs(pdu->tcp_port);
     int size = 4 + (get_num_words(pdu->server_name_length, 4) * 4);
     pdu->server_name = calloc((size_t) size, sizeof(uint32_t));
     read_from_fd(fd, pdu->server_name, pdu->server_name_length);
