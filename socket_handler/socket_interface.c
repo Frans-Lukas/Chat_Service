f65225ac684903557f32dc949a2fb8edc3688e32
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2018-10-09 10:32:55 +0200

    added struct

--- socket_handler/socket_interface.c
+++ socket_handler/socket_interface.c
@@ -5,7 +5,7 @@
 #include <pdu_handler/pdu_helper.h>
 #include "socket_interface.h"
 #include "socket_helper.h"
-
+#include <poll.h>
 
 int write_pdu_to_socket(PDU* pdu, int socket){
     char* data;
@@ -14,10 +14,25 @@ int write_pdu_to_socket(PDU* pdu, int socket){
     socket_single_write_to(socket, data);
 }
 
-PDU* read_pdu_from_socket(int socket){
+PDU** read_pdu_from_socket(int socket[], int size){
 
     //if poll says socket is readable, read from socket.
-
-    PDU* data = pdu_deserialize_next(socket);
+    struct pollfd fd[size];
+    for (int i = 0; i < size; ++i) {
+        fd[i].fd = socket[i];
+        fd[i].events = POLLIN;
+    }
+    int timeout;
+    timeout = 100;
+    poll(fd, (nfds_t) size, timeout);
+    PDU* data[size];
+    for (int j = 0; j < size; ++j) {
+        if(fd[j].revents == POLLIN){
+            data[j] = pdu_deserialize_next(socket[j]);
+        } else{
+            data[j] = NULL;
+        }
+    }
     return data;
 }
+
