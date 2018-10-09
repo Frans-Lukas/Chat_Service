411b0fd1444a8f2809de14720cdf3f6d5dd47ce4
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2018-10-09 10:02:03 +0200

    started working on socket interface

--- socket_handler/socket_interface.c
+++ socket_handler/socket_interface.c
@@ -2,4 +2,22 @@
 // Created by c16fld on 2018-10-05.
 //
 
+#include <pdu_handler/pdu_helper.h>
 #include "socket_interface.h"
+#include "socket_helper.h"
+
+
+int write_pdu_to_socket(PDU* pdu, int socket){
+    char* data;
+    pdu_serialize(pdu, &data);
+    //if poll says socket is writable, write to socket.
+    socket_single_write_to(socket, data);
+}
+
+PDU* read_pdu_from_socket(int socket){
+
+    //if poll says socket is readable, read from socket.
+
+    PDU* data = pdu_deserialize_next(socket);
+    return data;
+}
--- socket_handler/socket_interface.h
+++ socket_handler/socket_interface.h
@@ -5,9 +5,4 @@
 #ifndef CHAT_SERVICE_SOCKET_INTERFACE_H
 #define CHAT_SERVICE_SOCKET_INTERFACE_H
 
-
-void socket_add_pdu_to_send_queue(quque* queue, char* pdu, int length_of_pdu);
-void socket_add_pdu_to_read_queue(quque* queue, );
-
-
 #endif //CHAT_SERVICE_SOCKET_INTERFACE_H
