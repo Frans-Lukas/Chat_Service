fca48683d230c1bff6745701672cd246d5def570
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2018-10-17 11:34:46 +0200

    fix

--- chat_service_server.h
+++ chat_service_server.h
@@ -5,7 +5,15 @@
 #ifndef CHAT_SERVICE_CHAT_SERVICE_SERVER_H
 #define CHAT_SERVICE_CHAT_SERVICE_SERVER_H
 
+#include "error_handling.h"
+#include <socket_handler/client_list.h>
 
+typedef struct server_accepting_arguments{
+    int server_socket;
+    client_list* cl;
+} server_accepting_arguments;
 
+void server_keep_accepting_clients(void* args);
+void server_run_server();
 
 #endif //CHAT_SERVICE_CHAT_SERVICE_SERVER_H
