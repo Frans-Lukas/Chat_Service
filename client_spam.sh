272021f85c5c1460cfc1ffbfbe4e745d87ecbf4b
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2018-10-24 14:46:00 +0200

    minor

--- client_spam.sh
+++ client_spam.sh
@@ -8,7 +8,7 @@ cmake .
 make Client
 END=230
 for ((i=1;i<=END;i++)); do
-    ./Client Bot$i cs short.cs.umu.se 6969 < first_message_before_exit.txt > /dev/null 2>&1 &
+    ./Client Bot$i cs long.cs.umu.se 1337 < first_message_before_exit.txt > /dev/null 2>&1 &
 done
 sleep 5
 pkill Client -9
